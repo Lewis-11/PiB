e582e6da921ee91ce90c629c3f62784f2e38169c
Lewis-11 <100383365@alumnos.uc3m.es>
2022-11-03 15:02:17 +0100

    second mst order function

--- msa/src/gusfields.rs
+++ msa/src/gusfields.rs
@@ -1,6 +1,6 @@
 use crate::fasta::Alignment;
 
-pub fn get_center_string(adjacency_matrix: &Vec<Vec<Alignment>>, maximize: bool) -> usize {
+pub fn get_center_string(adjacency_matrix: &Vec<Vec<i32>>, maximize: bool) -> usize {
     let n = adjacency_matrix.len();
     let mut center_string = 0;
     let mut max_score = 0;
@@ -8,7 +8,7 @@ pub fn get_center_string(adjacency_matrix: &Vec<Vec<Alignment>>, maximize: bool)
     for i in 0..n {
         let mut score = 0;
         for j in 0..n {
-            score += adjacency_matrix[i][j].score;
+            score += adjacency_matrix[i][j];
         }
         if maximize && score > max_score {
             max_score = score;
@@ -53,55 +53,66 @@ fn merge_clusters(mut cluster1: Vec<Vec<u8>>, mut cluster2: Vec<Vec<u8>>, cluste
 
 }
 
-pub(crate) fn gusfield_alignment(adjacency_matrix: &Vec<Vec<Alignment>>) -> Vec<Vec<u8>> {
-    let n = adjacency_matrix.len();
-    let center_string = get_center_string(adjacency_matrix, true);
-    let mut matrix = Vec::new();
-    matrix.push(adjacency_matrix[center_string][center_string].sequences[0].sequence.as_bytes().to_vec());
-    for seq in 0..n {
-        if seq == center_string { continue };
-        let mut i:usize = 0;
-        let mut j:usize = 0;
-        let mut new_row = Vec::new();
-        let center_seq = adjacency_matrix[center_string][seq].sequences[0].sequence.as_bytes().to_vec();
-        let c_seq =      adjacency_matrix[seq][center_string].sequences[0].sequence.as_bytes().to_vec();
-        while i < matrix[0].len() && j < center_seq.len() {
-
-            // Case 1:
-            if matrix[0][i] == center_seq[j] {
-                new_row.push(c_seq[j]);
-                i += 1;
-                j += 1;
-            }
-
-            // Case 2:
-            else if matrix[0][i] == b'-' && center_seq[j] != b'-' {
-                new_row.push(b'-');
-                i += 1;
-            }
-
-            // Case 3:
-            else if matrix[0][i] != b'-' && center_seq[j] == b'-' {
-                insert_gap_at(&mut matrix, i);
-                new_row.push(c_seq[j]);
-                i += 1;
-                j += 1;
-            }
-        }
-        while j < center_seq.len() {
-            insert_gap_at(&mut matrix, i);
-            new_row.push(c_seq[j]);
-            i += 1;
-            j += 1;
-        }
-        while i < matrix[0].len() {
-            new_row.push(b'-');
-            i += 1;
-        }
-        matrix.push(new_row);
+pub fn gusfield_mst(score_matrix: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let center_string = get_center_string(score_matrix, true);
+    let n = score_matrix.len();
+    let mut merge_order : Vec<Vec<i32>>= Vec::new();
+    for i in 0..n {
+        if i == center_string { continue; };
+        merge_order.push(vec![center_string as i32, i as i32]);
     }
-    matrix
+    return merge_order;
 }
+
+// pub(crate) fn gusfield_alignment(adjacency_matrix: &Vec<Vec<Alignment>>) -> Vec<Vec<u8>> {
+//     let n = adjacency_matrix.len();
+//     let center_string = get_center_string(adjacency_matrix, true);
+//     let mut matrix = Vec::new();
+//     matrix.push(adjacency_matrix[center_string][center_string].sequences[0].sequence.as_bytes().to_vec());
+//     for seq in 0..n {
+//         if seq == center_string { continue };
+//         let mut i:usize = 0;
+//         let mut j:usize = 0;
+//         let mut new_row = Vec::new();
+//         let center_seq = adjacency_matrix[center_string][seq].sequences[0].sequence.as_bytes().to_vec();
+//         let c_seq =      adjacency_matrix[seq][center_string].sequences[0].sequence.as_bytes().to_vec();
+//         while i < matrix[0].len() && j < center_seq.len() {
+//
+//             // Case 1:
+//             if matrix[0][i] == center_seq[j] {
+//                 new_row.push(c_seq[j]);
+//                 i += 1;
+//                 j += 1;
+//             }
+//
+//             // Case 2:
+//             else if matrix[0][i] == b'-' && center_seq[j] != b'-' {
+//                 new_row.push(b'-');
+//                 i += 1;
+//             }
+//
+//             // Case 3:
+//             else if matrix[0][i] != b'-' && center_seq[j] == b'-' {
+//                 insert_gap_at(&mut matrix, i);
+//                 new_row.push(c_seq[j]);
+//                 i += 1;
+//                 j += 1;
+//             }
+//         }
+//         while j < center_seq.len() {
+//             insert_gap_at(&mut matrix, i);
+//             new_row.push(c_seq[j]);
+//             i += 1;
+//             j += 1;
+//         }
+//         while i < matrix[0].len() {
+//             new_row.push(b'-');
+//             i += 1;
+//         }
+//         matrix.push(new_row);
+//     }
+//     matrix
+// }
 
 
 
--- msa/src/wasm.rs
+++ msa/src/wasm.rs
@@ -16,19 +16,44 @@ mod msa;
 #[wasm_bindgen]
 pub fn wasm_serialize_fasta_string(fasta_string: String) -> JsValue {
     let sequences = parse_fasta_string(fasta_string);
-    serde_wasm_bindgen::to_value(&sequences).unwrap()
+    serde_wasm_bindgen::to_value(&sequences).unwrap_or(JsValue::NULL)
 }
 
 #[wasm_bindgen]
 pub fn wasm_serialize_submatrix(submatrix_string: String) -> JsValue {
     let submatrix_cost = parse_submatrix_string(submatrix_string);
-    serde_wasm_bindgen::to_value(&submatrix_cost).unwrap()
+    serde_wasm_bindgen::to_value(&submatrix_cost).unwrap_or(JsValue::NULL)
 }
+
+/**
+ * @param {FastaSequence} seq1 {name: string, sequence: string}
+ * @param {FastaSequence} seq1 {name: string, sequence: string}
+ * @param {HashMap [u8][u8] -> i32} sub_matrix
+ * @param {number} gap_cost
+ * @param {boolean} maximize
+ * @returns {Alignment} {sequences: [FastaSequence, FastaSequence], score: number}
+ */
+#[wasm_bindgen]
+pub fn wasm_pairwise_alignment(seq1: FastaSequence, seq2: FastaSequence, sub_matrix: &HashMap<u8, HashMap<u8, i32>>, gap_cost: i32, maximize: bool) -> JsValue {
+    let alignment = alignment::pairwise_alignment(&seq1, &seq2, sub_matrix, gap_cost, maximize).unwrap_or(Alignment::new(vec![], 0));
+    serde_wasm_bindgen::to_value(&alignment).unwrap_or(JsValue::NULL)
+}
+
+/**
+ * @param {Vec<Vec<i32>>} score_matrix
+ * @returns {Vec<Vec<i32>>} merges list containing pairs of indices to merge
+ */
+#[wasm_bindgen]
+pub fn wasm_gusfield_mst(score_matrix: Vec<Vec<i32>>) -> JsValue {
+    let merges = gusfields::gusfield_mst(&score_matrix);
+    serde_wasm_bindgen::to_value(&merges).unwrap_or(JsValue::NULL)
+}
+
 
 #[wasm_bindgen]
 pub fn wasm_gusfields(fasta_string: String, submatrix_string: String, gap_cost: i32, maximize: bool) -> JsValue {
     let records = parse_fasta_string(fasta_string);
     let sm = parse_submatrix_string(submatrix_string);
     let alignment = gusfield_msa(&records, &sm, gap_cost, maximize).unwrap();
-    serde_wasm_bindgen::to_value(&alignment).unwrap()
+    serde_wasm_bindgen::to_value(&alignment).unwrap_or(JsValue::NULL)
 }
