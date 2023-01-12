4895e339cd03ae7d06452ab9018dd6ca351d2b7d
Lewis-11 <100383365@alumnos.uc3m.es>
2023-01-12 02:00:49 +0100

    working

--- msa/src/wasm.rs
+++ msa/src/wasm.rs
@@ -66,26 +66,26 @@ pub fn wasm_kruskal_mst(score_matrix: String) -> JsValue {
 
 #[wasm_bindgen]
 pub fn msa_wasm(fasta: String, substitution_matrix: String, gap_cost: i32, algorithm: String) -> JsValue {
-    let output: Option<(Vec<Vec<Vec<Vec<u8>>>>, i32)> = msa(&substitution_matrix, gap_cost, false, &fasta, &algorithm);
+    let output: (Vec<Vec<Vec<Vec<u8>>>>, i32) = msa(&substitution_matrix, gap_cost, false, &fasta, &algorithm).unwrap();
     let steps: Vec<Vec<Vec<Vec<u8>>>> = output.0;
     let score = output.1;
-    let mut js_output: &str = "";
+    let mut js_output: String = String::from("");
     for step in steps {
         for cluster in step {
             for seq in cluster {
-                js_output = js_output + seq.to_string().as_str() + "&";
+                js_output = js_output + std::str::from_utf8(&seq).unwrap();
             }
             // remove last &
-            js_output = js_output.trim_end_matches('&');
+            js_output.pop();
             js_output = js_output + "$";
         }
         // remove last $
-        js_output = js_output.trim_end_matches('$');
+        js_output.pop();
         js_output = js_output + "#";
     }
     // add score at the end
     js_output = js_output + score.to_string().as_str();
-    return JsValue::from_str(js_output);
+    return JsValue::from_str(js_output.as_str());
 }
 
 
