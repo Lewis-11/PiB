211426d576be37907259e10cb1f7b3fe02788800
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2023-01-13 18:56:05 +0100

    Merge visualization. In the end we had to do some compromises.

--- msa/www/index.js
+++ msa/www/index.js
@@ -2,15 +2,23 @@ import "bulma";
 import "./index.css";
 import * as msa from "../pkg/msa";
 import * as CONST from './constants';
+import "./relativeplugin";
 
 (() => {
-
-	const CANVAS_FONT_SIZE = 32
+	
+	let paused = false
+	let stepEnabled = false
+	let stepRequest = false
+	let requestStop = false
+	let visRunning = false
 
 	/**********************************************************************
 	 * HTML elements
 	 */
 	let buttonPlay = document.getElementById("button-play");
+	let buttonToggle = document.getElementById("button-toggle");
+	let buttonStep = document.getElementById("button-step");
+	let buttonReset = document.getElementById("button-reset");
 	let fieldFasta = document.getElementById("field-fasta");
 	let fieldCostMatrix = document.getElementById("field-cost-matrix");
 	let fieldGapCost = document.getElementById("field-gap-cost");
@@ -31,16 +39,18 @@ import * as CONST from './constants';
 	/**********************************************************************
 	 * Canvas setup
 	 */
-	let displayCanvas = document.getElementById("display-canvas")
-	displayCanvas.width = displayCanvas.clientWidth;
-	displayCanvas.height = displayCanvas.clientHeight;
+	const ratio = window.devicePixelRatio;
+	let canvas = document.getElementById("display-canvas")
+    canvas.style.width = canvas.clientWidth + "px";
+    canvas.style.height = canvas.clientHeight + "px";
+	canvas.width = canvas.clientWidth * ratio
+	canvas.height = canvas.clientHeight * ratio
+    canvas.getContext("2d").scale(ratio, ratio);
 
-	let ctx = displayCanvas.getContext("2d");
-	ctx.font = `${CANVAS_FONT_SIZE}px Courier New`;
-	ctx.fillStyle = "white";
-	ctx.textBaseline = 'middle';
-	ctx.textAlign = "center";
-	ctx.fillText("Empty canvas", displayCanvas.width / 2, displayCanvas.height / 2);
+	let stage = new createjs.Stage("display-canvas")
+	createjs.RelativePlugin.install();
+	createjs.Ticker.setFPS(60);
+	createjs.Ticker.addEventListener("tick", stage);
 
 	/**********************************************************************
 	 * Utils
@@ -58,18 +68,77 @@ import * as CONST from './constants';
 			reader.readAsText(file);
 		})
 	}
+	
+	function clusterStepParser(stringToParse) {
+		let result = []
+		let stepsAndScore = stringToParse.split(CONST.DELIMITER_STEP)
+		
+		for (let x = 0; x < stepsAndScore.length - 1; x += 1) {
+			let clusters = stepsAndScore[x].split(CONST.DELIMITER_CLUSTER)
+			result.push([
+				clusters[0].split(CONST.DELIMITER_LINE),
+				clusters[1].split(CONST.DELIMITER_LINE),
+				clusters[2].split(CONST.DELIMITER_LINE),
+			])
+		}
+		
+		return {
+			steps: result,
+			score: stepsAndScore[stepsAndScore.length - 1]
+		}
+	}
+
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+	
+	function createBaseInCanvas(letter, fontSize, x, y) {
+		let base = new createjs.Text(letter, fontSize + "px Courier", "#FFFFFF");
+		base.x = x; base.y = y;
+		base.baseline = "middle"
+		base.textAlign = "center"
+		return base
+	}
+	
+	function createPerCharacterUiAlignment(alignment, fontSize) {
+		let uiElements = []
+		for (let i = 0; i < alignment.data.length; i++) {
+			uiElements.push([])
+			for (let j = 0; j < alignment.data[0].length; j++) {
+				let xLetterShift = fontSize * CONST.X_CHAR_SHIFT_MULTIPLIER;
+				let yLetterShift = fontSize * CONST.Y_CHAR_SHIFT_MULTIPLIER;
+				let x = alignment.pos.x + xLetterShift * j; let y = alignment.pos.y + yLetterShift * i;
+				let base = createBaseInCanvas(alignment.data[i][j], fontSize, x, y)
+				uiElements[i].push(base)
+				stage.addChild(base)
+			}
+		}
+		return uiElements
+	}
+	
+	function createPerLineUiAlignment(alignment, fontSize) {
+		let uiElements = []
+		for (let i = 0; i < alignment.data.length; i++) {
+			let yLetterShift = fontSize * CONST.Y_CHAR_SHIFT_MULTIPLIER;
+			let x = alignment.pos.x; let y = alignment.pos.y + yLetterShift * i;
+			let base = createBaseInCanvas(alignment.data[i], fontSize, x, y)
+			uiElements.push(base)
+			stage.addChild(base)
+		}
+		return uiElements
+	}
 
 	/**********************************************************************
-	 * Button listeners
+	 * Visualization
 	 */
-	buttonPlay.onclick = async function (e) {
+	async function visualization() {
 		let fastaString;
 		let costmatrixString;
 		let gapCost = fieldGapCost.value;
 		let maximize = fieldMaximize.value;
 		let algorithm = fieldAlgorithm.value;
 		let stepInterval = fieldStepInterval.value;
-
+		
 		// Retrieve text from files (or not)
 		if (fileSelectorFasta.files.length > 0) {
 			fastaString = await readFileAsync(fileSelectorFasta.files[0])
@@ -82,21 +151,151 @@ import * as CONST from './constants';
 		} else {
 			costmatrixString = fieldCostMatrix.value;
 		}
+		
+		let result = msa.msa_wasm(fastaString, costmatrixString, gapCost, algorithm)
+		let parsedResult = clusterStepParser(result)
 
-		let result = msa.wasm_gusfields(fastaString, costmatrixString, gapCost, maximize)
+		let offScreenXOffset = canvas.clientWidth * window.devicePixelRatio * 2;
+		let offScreenYOffset = canvas.clientHeight * window.devicePixelRatio * 2;
+		
+		let alignmentCluster1, alignmentCluster2, alignmentResultingCluster
+		
+		visRunning = true;
+		
+		for (let idx = 0; idx < parsedResult.steps.length; idx += 1) {
 
-		ctx.clearRect(0, 0, displayCanvas.width, displayCanvas.height);
+			if (requestStop) { stage.removeAllChildren(); break; }
+			while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+			
+			const curr = parsedResult.steps[idx]
+			
+			// Draw current step
+			
+			// Compute the font size and (x,y) for each alignemnt.
+			// Big alignments *should* be able to fit.
+			let fontSizeCluster1 = Math.floor(canvas.clientWidth / window.devicePixelRatio * 0.8 / curr[0][0].length) * 8;
+			let fontSizeCluster2 = Math.floor(canvas.clientWidth / window.devicePixelRatio * 0.8 / curr[1][0].length) * 8;
+			let fontSizeResultingCluster = Math.floor(canvas.clientWidth / window.devicePixelRatio * 0.8 / curr[2][0].length) * 8;
 
-		let baseCoords = {
-			width: displayCanvas.width / 2,
-			height: displayCanvas.height / 2 - (result.sequences.length / 2) * CANVAS_FONT_SIZE
+			// Make sure font size isn't too big
+			fontSizeCluster1 = Math.min(32, fontSizeCluster1);
+			fontSizeCluster2 = Math.min(32, fontSizeCluster2);
+			fontSizeResultingCluster = Math.min(32, fontSizeResultingCluster);
+			
+			alignmentCluster1 = {pos: {x: canvas.clientWidth, y: canvas.clientHeight * 0.33 * 2 + offScreenYOffset}, data: curr[0]}
+			alignmentCluster2 = {pos: {x: canvas.clientWidth, y: canvas.clientHeight * 0.66 * 2 + offScreenYOffset}, data: curr[1]}
+			alignmentResultingCluster = {pos: {x: canvas.clientWidth + offScreenXOffset, y: canvas.clientHeight}, data: curr[2]}
+	
+			let uiAlignmentCluster1 = createPerLineUiAlignment(alignmentCluster1, fontSizeCluster1)
+			let uiAlignmentCluster2 = createPerLineUiAlignment(alignmentCluster2, fontSizeCluster2)
+			let uiAlignmentResultingCluster = createPerLineUiAlignment(alignmentResultingCluster, fontSizeResultingCluster)
+			
+			alignmentCluster1.pos.y -= offScreenYOffset
+			for (const row of uiAlignmentCluster1) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ y: "-" + offScreenYOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+	
+			alignmentCluster2.pos.y -= offScreenYOffset
+			for (const row of uiAlignmentCluster2) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ y: "-" + offScreenYOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+	
+			await sleep(stepInterval)
+	
+			alignmentCluster1.pos.x -= offScreenXOffset
+			for (const row of uiAlignmentCluster1) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ x: "-" + offScreenXOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+	
+			alignmentCluster2.pos.x -= offScreenXOffset
+			for (const row of uiAlignmentCluster2) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ x: "-" + offScreenXOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+	
+			alignmentResultingCluster.pos.x -= offScreenXOffset
+			for (const row of uiAlignmentResultingCluster) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ x: "-" + offScreenXOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+	
+			await sleep(stepInterval)
+			
+			while (stepEnabled && !stepRequest) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+			stepRequest = false;
+	
+			var lastStepOffset = offScreenYOffset
+
+			// Last index is different
+			if (idx == parsedResult.steps.length - 1) {
+				lastStepOffset = alignmentResultingCluster.pos.y - 100
+			}
+
+			alignmentResultingCluster.pos.y -= lastStepOffset
+			for (const row of uiAlignmentResultingCluster) {
+				while (paused) { await sleep(CONST.PAUSE_SLEEP_TIMEOUT) }
+				createjs.Tween.get(row, { loop: false })
+					.to({ y: "-" + lastStepOffset }, stepInterval, createjs.Ease.getPowInOut(4))
+			}
+			
 		}
 
-		ctx.fillText(`The score is ${result.score}`, baseCoords.width, baseCoords.height);
+		visRunning = false;
+		stepEnabled = false;
+		
+		await sleep(stepInterval)
 
-		result.sequences.forEach((s, i) => {
-			ctx.fillText(`${s.name}: ${s.sequence}`, baseCoords.width, baseCoords.height + (i + 1) * CANVAS_FONT_SIZE);
-		});
+		let uiScore = new createjs.Text("Score: " + parsedResult.score, "48px Courier", "#FFFFFF");
+		uiScore.baseline = "middle"
+		uiScore.textAlign = "center"
+		uiScore.x = offScreenXOffset / 4; uiScore.y = alignmentResultingCluster.pos.y + 200;
+		uiScore.alpha = 0;
+		stage.addChild(uiScore)
+		createjs.Tween.get(uiScore).to({alpha:1}, stepInterval);
+	}
+
+	/**********************************************************************
+	 * Button listeners
+	 */
+	buttonPlay.onclick = function (e) {
+		if (stepEnabled) {
+			stepEnabled = false;
+		} else if (visRunning) {
+			return;
+		} else {
+			visualization()
+		}
+	}
+	
+	buttonToggle.onclick = function (e) {
+		createjs.Ticker.paused = !createjs.Ticker.paused
+		paused = createjs.Ticker.paused
+	}
+	
+	buttonStep.onclick = function (e) {
+		stepEnabled = stepRequest = true;
+
+		if (visRunning == false) {
+			stepRequest = false;
+			visualization()
+		}
+		
+	}
+	
+	buttonReset.onclick = function (e) {
+		if (visRunning) {
+			requestStop = true;
+		} else {
+			stage.removeAllChildren();
+		}
 	}
 
 	/**********************************************************************
